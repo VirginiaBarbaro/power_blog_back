d4e29ba77bcef7059a1da2fb914965dcdb2174b9
VirginiaBarbaro <virginiabarbaro@icloud.com>
2023-06-28 18:35:14 -0300

    Deleting the admin Model

--- src/models/db.ts
+++ src/models/db.ts
@@ -3,7 +3,6 @@ import User from "./User";
 import Article from "./Article";
 import Comment from "./Comment";
 import Favourite from "./Favourite";
-import Admin from "./Admin";
 
 const sequelize = new Sequelize(
   //   process.env.DB_NAME,
@@ -25,6 +24,5 @@ User.initModel(sequelize);
 Article.initModel(sequelize);
 Comment.initModel(sequelize);
 Favourite.initModel(sequelize);
-Admin.initModel(sequelize);
 
-export default { sequelize, User, Article, Comment, Favourite, Admin };
+export default { sequelize, User, Article, Comment, Favourite };
