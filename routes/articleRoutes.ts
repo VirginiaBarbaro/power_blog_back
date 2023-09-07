71681e0bea193f77cf6ae9d03dbc06b7eff15c96
VirginiaBarbaro <virginiabarbaro@icloud.com>
2023-09-07 16:30:25 -0300

    multer name

--- routes/articleRoutes.ts
+++ routes/articleRoutes.ts
@@ -7,7 +7,7 @@ import {
   destroyArticle,
   getArticlesByUser,
 } from "../controllers/articleController";
-import upload from "../libs/multer";
+import multer from "../libs/multer";
 import { expressjwt } from "express-jwt";
 const router: Router = Router();
 
@@ -20,14 +20,14 @@ router.get("/user/:userId", getArticlesByUser);
 router.post(
   "/",
   expressjwt({ secret: `${process.env.JWT_KEY}`, algorithms: ["HS256"] }),
-  upload.single("image"),
+  multer.single("image"),
   createArticle
 );
 
 router.patch(
   "/:id",
   expressjwt({ secret: `${process.env.JWT_KEY}`, algorithms: ["HS256"] }),
-  upload.single("image"),
+  multer.single("image"),
   updateArticle
 );
 
